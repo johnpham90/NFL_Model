c18a568d970c2bbcc8e4223505f4263e72c01d1e
johnpham90 <ja.pham131@gmail.com>
2024-12-13 16:19:07 -0500

    Importing db connection
    Imported db connetion utils for each file

--- src/features/advancedmetric_level_features.py
+++ src/features/advancedmetric_level_features.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from src.utils.db_utils import get_connection, execute_query
 
 def advancedmetric_features(df):
     '''Create advancedmetric-level features'''
--- src/features/player_level_features.py
+++ src/features/player_level_features.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from src.utils.db_utils import get_connection, execute_query
 
 def player_features(df):
     '''Create player-level aggregate features'''
